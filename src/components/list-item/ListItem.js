ee151b98b5d6ab724de144a6e95648e957b5f2d5
sunilk121 <sunilkunwor63@gmail.com>
2022-03-29 14:59:30 +1100

    updated Listitem.js

--- src/components/list-item/ListItem.js
+++ src/components/list-item/ListItem.js
@@ -5,27 +5,17 @@ export const ListItem = ({ movie, fun, btnDelete }) => {
   return (
     <Row>
       <Col>
-        <img src={movie?.poster} width="100%" alt="" />
+        <img src={movie?.Poster} width="100%" alt="" />
       </Col>
       <Col>
-        <h3>{movie?.title}</h3>
+        <h3>{movie?.Title}</h3>
         <h3>Rating: {movie?.imdbRating}</h3>
-        {btnDelete ? (
-          <div className="d-grid gap-2">
-            <Button variant="danger" onClick={() => fun(movie.imdbID)}>
-              Delete
-            </Button>
-          </div>
-        ) : (
-          <div className="d-flex justify-content-between">
-            <Button variant="info" onClick={() => fun("happy", movie)}>
-              Happy
-            </Button>
-            <Button variant="secondary" onClick={() => fun("lazy", movie)}>
-              Lazy
-            </Button>
-          </div>
-        )}
+
+        <div className="d-grid gap-2">
+          <Button variant="danger" onClick={() => fun(movie.imdbID)}>
+            Delete
+          </Button>
+        </div>
       </Col>
     </Row>
   );
